0ead852ffb349156e43bf73c0e03a2ceaba0c0f0
Klein <kleincoder@gmail.com>
2024-08-31 20:21:52 -0300

    removendo X

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,20 +2,10 @@ import { FaSquareXTwitter } from "react-icons/fa6";
 
 export function Footer() {
   return (
-    <footer className="bg-black text-white py-4 px-8 flex justify-between items-center gap-4">
+    <footer className="bg-black text-white py-4 px-8 flex justify-center items-center">
       <p className="text-xs">
         &copy; 2024 Techformando. Todos os direitos reservados.
       </p>
-      <a
-        href="https://x.com/Techformando"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <div className="cursor-pointer flex items-center gap-1">
-          <FaSquareXTwitter className="h-6 w-6" />
-          <p className="text-base">Techformando</p>
-        </div>
-      </a>
     </footer>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import { FaSquareXTwitter } from "react-icons/fa6";
 
 export function Header() {
   return (
-    <header className="flex flex-col items-center justify-center p-2 bg-black shadow-lg">
+    <header className="flex flex-col items-center justify-center p-4 bg-black shadow-lg">
       <Link href="/">
         <div className="flex items-center pb-2 gap-2">
           <BookOpenCheck className="w-11 h-11 text-white" />
@@ -14,19 +14,6 @@ export function Header() {
       <h2 className="text-base text-gray-100 text-center">
         Transformando informação em evolução contínua.
       </h2>
-      <div className="flex gap-3 items-center mt-4">
-        <h3 className="text-sm text-gray-400 text-center">Siga-nos</h3>
-        <a
-          href="https://x.com/Techformando"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className="cursor-pointer flex items-center gap-1">
-            <FaSquareXTwitter className="h-5 w-5 text-gray-400" />
-            <p className="text-sm text-gray-400">Techformando</p>
-          </div>
-        </a>
-      </div>
     </header>
   );
 }
